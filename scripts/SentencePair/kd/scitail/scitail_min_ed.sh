8c90a8a9db88e89c7d78f0a19bb066f775ca57e1
tmp0810 <truongminhphuc08102005@gmail.com>
2025-10-30 20:08:16 +0700

    Update scitail_min_ed.sh

--- scripts/SentencePair/kd/scitail/scitail_min_ed.sh
+++ scripts/SentencePair/kd/scitail/scitail_min_ed.sh
@@ -16,7 +16,7 @@ DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE \
 
 # model
 BASE_PATH=/LLM2Vec_Distillation
-CKPT_NAME="bert"
+CKPT_NAME="Tinybert6L_MinED_Scitail"
 CKPT_PATH="${BASE_PATH}/model_hub/${CKPT_NAME}"
 TEACHER_MODEL_NAME="LLM2Vec"
 TEACHER_MODEL_PATH="${BASE_PATH}/model_hub/${TEACHER_MODEL_NAME}" # GẮN LINK MODEL CHECKPOINT VÀO ĐÂY# data
